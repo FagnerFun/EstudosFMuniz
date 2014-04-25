8e581c20490bff335eb4a133e990eca79c799dab
patric oliveira <patric_c15@hotmail.com>
2014-04-25 00:14:06 -0300

    LOOOOOOOOl

---
+++ Projetos/src/Cliente.java
@@ -0,0 +1,20 @@
+
+public class Cliente extends Pessoa {
+	
+	int cartao;
+	float datacadastro;
+	
+public void cliente (){
+	
+	
+}
+public void compra (int novacompra){
+	
+}
+
+public void reclama (int novareclamação){
+	
+}
+
+
+}
