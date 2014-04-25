c4a87f8a3ba15066aab4ae5a74952449df90177e
digao15 <digao_c13@hotmail.com>
2014-04-25 00:27:54 -0300

    finalizado classe funcionario

---
+++ Projetos/src/Funcionario.java
@@ -0,0 +1,24 @@
+
+public class Funcionario extends Pessoa {
+	
+	//atributos
+	
+	private int cargo;
+	private int horasTrabalhos;
+	private int departamento;
+	
+	//Metodos
+	
+	public void funcionario()
+	{
+		
+	}
+	public void trabalha()
+	{
+		
+	}
+	public void almoca()
+	{
+	
+	}
+}
